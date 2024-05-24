30d5f69ad3c0b6f3e4a4f26ac26f2357c2bc60e1
orket <developerorket@gmail.com>
2024-05-24 16:37:20 +0300

    lc

--- go-bank/go.mod
+++ go-bank/go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/lib/pq v1.10.9
 )
 
-require github.com/golang-jwt/jwt v3.2.2+incompatible 
+
+require github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
--- go-bank/handler.go
+++ go-bank/handler.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func HandlerCheckCreateAccReqOnCreate(accrequest *CreateAccountRequest, w http.ResponseWriter) error {
@@ -17,6 +20,25 @@ func HandlerCheckCreateAccReqOnCreate(accrequest *CreateAccountRequest, w http.R
 func WithJWT(handlerFunc http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("middleware is working")
+		tokenString := r.Header.Get("Authorization")
+		_, err := ValidateJwt(tokenString)
+		if err != nil {
+			log.Println(err)
+		}
 		handlerFunc(w, r)
 	}
+}
+
+func ValidateJwt(tokenString string) (*jwt.Token, error) {
+
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte("my_secret_key"), nil
+	})
+
 }
