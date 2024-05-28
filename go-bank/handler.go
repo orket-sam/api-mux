1c077d603690c41a4505f6aeb5b01838bb5ef266
orket <developerorket@gmail.com>
2024-05-28 16:56:51 +0300

    lc

--- go-bank/handler.go
+++ go-bank/handler.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 
-	"github.com/golang-jwt/jwt/v5"
+	jwt "github.com/golang-jwt/jwt/v5"
 )
 
 func HandlerCheckCreateAccReqOnCreate(accrequest *CreateAccountRequest, w http.ResponseWriter) error {
@@ -17,28 +17,24 @@ func HandlerCheckCreateAccReqOnCreate(accrequest *CreateAccountRequest, w http.R
 	return nil
 }
 
-func WithJWT(handlerFunc http.HandlerFunc) http.HandlerFunc {
+func WithJwt(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("middleware is working")
-		tokenString := r.Header.Get("Authorization")
-		_, err := ValidateJwt(tokenString)
-		if err != nil {
-			log.Println(err)
-		}
-		handlerFunc(w, r)
+		tokenString := r.Header.Get("x-token")
+		f(w, r)
 	}
 }
 
 func ValidateJwt(tokenString string) (*jwt.Token, error) {
+	secret := os.Getenv("secret")
+	return jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
 
-	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			println("Invalid signing")
+
+			return nil, fmt.Errorf("invalid signing method used")
+
 		}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return []byte("my_secret_key"), nil
+		println("valid signing")
+		return []byte(secret), nil
 	})
-
 }
